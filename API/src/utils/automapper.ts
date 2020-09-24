8f39dd163664e0e497404973014cd38c42e48d89
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2020-09-23 23:11:51 -0300

    Creates automapper

--- API/src/utils/automapper.ts
+++ API/src/utils/automapper.ts
@@ -5,9 +5,24 @@ import _ from 'lodash';
  * @param origin The original source of data
  * @param destination The object that will receive the data
  */
-export function autoMapper<T>(origin: T, destination: T) {
+export function autoMapper<T>(
+  origin: T,
+  destination: T,
+  upper = false,
+  purge = false
+) {
   Object.keys(destination).forEach((key) => {
-    // @ts-expect-error
-    destination[_.toLower(key)] = origin[_.toLower(key)];
+    if (
+      !origin[upper ? (_.toUpper(key) as keyof T) : (_.toLower(key) as keyof T)]
+    ) {
+      if (purge) {
+        delete destination[_.toLower(key) as keyof T];
+      }
+    } else {
+      destination[_.toLower(key) as keyof T] =
+        origin[
+          upper ? (_.toUpper(key) as keyof T) : (_.toLower(key) as keyof T)
+        ];
+    }
   });
 }
